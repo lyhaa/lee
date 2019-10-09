17888d0b2d0b9dbff1b6d6603decc626cf0929e9
unknown <2715757308@qq.com>
2019-10-09 10:59:31 +0800

    day1

---
+++ day1/js/wf.js
@@ -0,0 +1,44 @@
+const aa = 123;
+
+// function deom(){
+//     
+// }
+// ;;(async()=>{
+//     await 
+//     await deom();
+//     
+//     await 
+// })()
+// const acca = 123;
+// 
+// function deom(){
+//     
+// }
+// ;;(async()=>{
+//     await 
+//     await deom();
+//     
+//     await 
+// })()
+// const abba = 123;
+// 
+// function deom(){
+//     
+// }
+// ;;(async()=>{
+//     await 
+//     await deom();
+//     
+//     await 
+// })()
+// function a(){
+//     
+// }
+// function c(){
+//     
+// }
+// function b(){
+//     
+// }
+// 
+// 
